92bd1ba63fea1a74152580876c697cf6b753def7
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-04-13 18:51:58 +0500

    one way of rendering error

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React, {useActionState} from 'react'
+import React, {useActionState, useState} from 'react'
 import {updateNameInDB} from "./api.js";
 
 function App () {
     const [name, actionFunction, isPending] = useActionState(updateName, JSON.parse(localStorage.getItem("name"))||'Anonymous user')
+    const [er, setEr] = useState(false)
     async function updateName(prevState,formAction) {
         try{
             return await updateNameInDB(formAction.get("name"))
         } catch(error){
             console.error(error.message)
+            setEr(error.message)
         }
     }
 
@@ -15,6 +17,7 @@ function App () {
         <>
             <p className={'username'}> Current user: {name} </p>
             {isPending && <p>Loading...</p>}
+            {<p style={{color:'red'}}>{er}</p>}
             <form action={actionFunction}>
                 <input
                 type="text"
